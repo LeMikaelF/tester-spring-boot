7c52b1f3c2b1aa1d4ae9817e1740d15163421a50
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2024-04-20 18:45:13 -0400

    Initial commit.

---
+++ src/main/java/com/mikaelfrancoeur/testerspringboot/TesterSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.mikaelfrancoeur.testerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesterSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesterSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mikaelfrancoeur/testerspringboot/TesterSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikaelfrancoeur.testerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesterSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
